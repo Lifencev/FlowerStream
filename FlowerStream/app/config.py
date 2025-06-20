c322285e501a4c561c4c5e0f4da81c8f9c419a59
Antony925 <152073152+Antony925@users.noreply.github.com>
2025-06-20 21:10:27 +0300

    Delete FlowerStream directory

--- FlowerStream/app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    DATABASE = os.getenv('DATABASE')
-    STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
-    STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY')
